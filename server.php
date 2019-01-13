67a61866965eb174bcf600436fcb0d4f8124fad8
Rahul gupta <37154619+rahulgupta1234@users.noreply.github.com>
2019-01-13 14:30:08 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$conn= mysqli_connect("localhost","root","","josh");
+	$conn= mysqli_connect("ec2-107-20-183-142.compute-1.amazonaws.com:5432","qgfekuimmdfdsy","5530568638cfa087281cbd3d0f1dc3e332bf1896a7df0de5b8a5e995dd119d40","d8mprcukat8cd0");
 
 	if(!$conn)die('Cannot Connect'.mysqli_error($conn));
 ?>
